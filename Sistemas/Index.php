b097d968036a792b845f73e97dad9d64a00853d1
MaluManarin <126784578+MaluManarin@users.noreply.github.com>
2023-06-15 09:52:55 -0300

    Add files via upload

---
+++ Sistemas/Index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+<body>
+    
+    <div class= "container text-center mt-5 mb-5">
+        <img src="Recursos/google-37.png" alt="" width = "80px">
+    </div>
+    <div class= "container text-center mb-5">
+    <h2> TELA DE LOGIN</h2>
+
+    </div>
+    <div class= "container text-center">
+        <form action="" method="post">
+            <label for="usuário">Usuário</label>
+            <input type="email" name="user" id="usuário">
+
+            <label for="senha">Senha</label>
+            <input type="password" name="senha" id="senha">
+
+            <input type="submit" value="Verificar">
+            <a href="Cadastro.php">Cadastrar</a>
+        </form>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</body>
+</html>
